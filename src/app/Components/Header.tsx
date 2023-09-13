381ac011cf05b1b284ac942f66efe43729a8c41f
Samira <samirasanchez8090@gmail.com>
2023-09-13 15:15:36 -0400

    header component defined

--- src/app/Components/Header.tsx
+++ src/app/Components/Header.tsx
@@ -4,22 +4,27 @@ import React from 'react'
 
 function Header() {
   return (
-    <header className='bg-blue-500 py-4'>
-        <div className='container mx-auto flex items-center justify-between'>
-            <div className='flex items-center'>
-                <img src='' className='h-12 w-12 mr-2'/>
-                <h1 className='text-white text-xl font-bold'>
-                    KKchan
-                </h1>
-                <nav className="space-x-4">
-                <a href="/">Home</a>
-                <a href="/board1">Board 1</a>
-                <a href="/board2">Board 2</a>
-                {/* TODO: Add more board links as needed */}
-                </nav>
-            </div>
-        </div>
-    </header>
+    <header className="bg-blue-500 py-4">
+  <div className="container mx-auto flex flex-col items-center justify-center">
+    <div className="flex items-center mb-4">
+      <img
+        src="" // Replace with the path to your logo image
+        alt="Logo"
+        className="h-12 w-12 mr-2"
+      />
+      <h1 className="text-white text-xl font-bold">KKchan</h1>
+    </div>
+    <div>
+      <nav className="space-x-4">
+        <a href="/">/kkhistory/</a>
+        <a href="/board1">/kkhelp/</a>
+        <a href="/board2">/kkgym/</a>
+        {/* TODO: Add more board links as needed */}
+      </nav>
+    </div>
+  </div>
+</header>
+
   )
 }
 
