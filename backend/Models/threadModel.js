8911bccfcbf20bfb9a8bf0e69c0677669b23fcfc
Samira <samirasanchez8090@gmail.com>
2023-09-17 12:19:29 -0400

    server updated and models defined

---
+++ backend/Models/threadModel.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const threadSchema = new mongoose.Schema({
+  title: String,
+  content: String,
+  author: String,
+  boardId: mongoose.Schema.Types.ObjectId,
+  created_at: Date,
+  image: String,
+});
+
+const Threads = mongoose.model('threads', threadSchema);
+
+// Create function for creating a new thread document
+Threads.createThread = async (body) => {
+  return await Threads.create(body);
+};
+
+module.exports = Threads;
+
--- backend/server.js
+++ backend/server.js
@@ -1,39 +1,89 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const app = express();
+const express = require('express')
+const mongoose = require('mongoose')
+const Threads = require('./Models/threadModel')
+const app = express()
 
 app.use(express.json())
+app.use(express.urlencoded({extended: false}))
 
 //routes
-app.get('/', (req, res) =>{
-    res.send('KKchan activo mmg')
+
+app.get('/', (req, res) => {
+    res.send('Hello NODE API')
 })
 
-app.get('/threads', async (req, res) =>{
-    try{
-        const thread = await Thread.find({});
-        res.status(200).json(thread)
-    } catch (error){
-        res.status(500).json({message: Error.message})
+app.get('/threads', async(req, res) => {
+    try {
+        const threads = await Threads.find({});
+        res.status(200).json(threads);
+    } catch (error) {
+        res.status(500).json({message: error.message})
     }
 })
 
-app.post('threads', async(req, res) =>{
-    try{
-        const thread = await Thread.create(req.body)
-        res.status(200).json(thread)
-    } catch (Error){
-        console.log(Error.message)
-        res.status(500).json({message: Error.message})
+app.get('/threads/:id', async(req, res) =>{
+    try {
+        const {id} = req.params;
+        const threads = await Threads.findById(id);
+        res.status(200).json(threads);
+    } catch (error) {
+        res.status(500).json({message: error.message})
     }
 })
 
-mongoose.connect("LINK TO CLUSTER")
+
+app.post('/threads', async(req, res) => {
+    try {
+        const threads = await Threads.create(req.body)
+        res.status(200).json(threads);
+        
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).json({message: error.message})
+    }
+})
+
+// update a product
+app.put('/threads/:id', async(req, res) => {
+    try {
+        const {id} = req.params;
+        const threads = await Threads.findByIdAndUpdate(id, req.body);
+        // we cannot find any product in database
+        if(!threads){
+            return res.status(404).json({message: `cannot find any product with ID ${id}`})
+        }
+        const updatedThread = await threads.findById(id);
+        res.status(200).json(updatedThread);
+        
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+})
+
+// delete a product
+
+app.delete('/threads/:id', async(req, res) =>{
+    try {
+        const {id} = req.params;
+        const threads = await Threads.findByIdAndDelete(id);
+        if(!threads){
+            return res.status(404).json({message: `cannot find any product with ID ${id}`})
+        }
+        res.status(200).json(threads);
+        
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+})
+
+mongoose.set("strictQuery", false)
+mongoose.
+connect('')
 .then(() => {
-    app.listen(3000, () => {
-        console.log('listening in port: 3000')
-    })
-    console.log("connected to database")
-}).catch(() => {
-    console.log(Error)
+    console.log('connected to MongoDB')
+    app.listen(3000, ()=> {
+        console.log(`Node API app is running on port 3000`)
+    });
+}).catch((error) => {
+    console.log(error)
 })
