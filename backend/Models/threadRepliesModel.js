3040e6fdbb1acd244e094667c3921fa0d52362a0
Samira <samirasanchez8090@gmail.com>
2023-09-19 16:16:48 -0400

    thread replies model

---
+++ backend/Models/threadRepliesModel.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const threadRepliesSchema = new mongoose.Schema({
+    creator: {type:String, default: "anonimo"},
+    content: String,
+    timestamp: {type: Date, default: Date.now},
+    image: {type: String, default: ""}
+});
+
+const ThreadReplies = mongoose.model('threadreplies', threadRepliesSchema);
+
+ThreadReplies.createReply = async (body) => {
+  return await ThreadReplies.create(body);
+};
+
+module.exports = ThreadReplies;
