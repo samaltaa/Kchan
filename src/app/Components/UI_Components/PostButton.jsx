96241a9e989481f6906d9dc90add0618a17c157f
Samira <samirasanchez8090@gmail.com>
2023-09-22 13:23:35 -0400

    resply form modal config

--- src/app/Components/UI_Components/PostButton.jsx
+++ src/app/Components/UI_Components/PostButton.jsx
@@ -1,10 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
+import ReplyForm from './ReplyForm'
 
 function Button() {
+  const [isModalVisible, setIsModalVisible] = useState(false);
+
+  const toggleModal = () =>{
+    setIsModalVisible(!isModalVisible)
+  };
+
+  let modal = null;
+
+  if (isModalVisible) {
+    modal = (
+      <div
+        className="modal"
+        style={{
+          position: "fixed",
+          top: 0,
+          right: 0,
+          bottom: 0,
+          width: "50%", // Adjust the width as needed
+          //backgroundColor: "white",
+          //boxShadow: "0 0 10px rgba(0, 0, 0, 0.3)",
+          zIndex: 1000,
+          overflowY: "auto",
+        }}
+      >
+          <ReplyForm/>
+      </div>
+    )
+  } 
   return (
-    <button className='button bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>
-      Responder
-    </button>
+    <div>
+      <button onClick={toggleModal} className='button bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>
+        Responder
+      </button>
+      {modal}
+    </div>
   )
 }
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
       <Header />
       <BoardList />
       <PopularThreads />
-      <ReplyForm/>
+      
       <Thread/>
     </div>
   );
